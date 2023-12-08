079656d32434baea86907bc0f9ca3d05ddde7d6a
Neel Kachhia <neel.kachhia13@gmail.com>
2023-12-08 15:44:04 -0500

    update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -23,4 +23,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Initial background image
   changeBackgroundImage();
+
+  if (Notification.permission !== "granted") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        // showNotification("Welcome!", "Thank you for granting permission.");
+        let notification = new Notification("Welcome to Career Connect", {
+          body: "Thank you for allowing our notifications.",
+        });
+      } else {
+        console.log("Permission denied for notifications");
+        // alert(
+        //   "Please allow notifications for this site. You can manage your settings in your browser."
+        // );
+      }
+    });
+  } else {
+    console.log("permission already granted!");
+  }
+});
+
+let notification, interval;
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    let leaveDate = new Date();
+    interval = setInterval(() => {
+      notification = new Notification("Come back please!!!", {
+        body: `You have been away since last ${Math.round(
+          (new Date() - leaveDate) / 1000
+        )} seconds.`,
+        tag: "Come back",
+      });
+    }, 5000);
+  } else {
+    if (interval) clearInterval(interval);
+    if (notification) notification.close();
+  }
 });
